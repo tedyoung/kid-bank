3c17f14b537b2a6f9d849763b201d1c79a3782a2
Ted M. Young <tedyoung@gmail.com>
2019-01-14 12:38:12 -0800

    Added few more tests for formatting money

--- src/test/java/com/learnwithted/kidbank/AccountControllerTest.java
+++ src/test/java/com/learnwithted/kidbank/AccountControllerTest.java
@@ -23,6 +23,14 @@ public class AccountControllerTest {
   }
 
   @Test
+  public void formatMoreThanOneDollarShouldHaveNoLeadingZeroes() throws Exception {
+    AccountController accountController = new AccountController(null);
+
+    assertThat(accountController.formatAsMoney(123))
+        .isEqualTo("$1.23");
+  }
+
+  @Test
   public void viewBalanceShouldDisplayBalanceOfGivenAccount() throws Exception {
     Account account = new Account();
     account.deposit(10);
