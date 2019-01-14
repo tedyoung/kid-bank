f6079807d568de2938e7b3ac8b86ba897a2666f4
Ted M. Young <tedyoung@gmail.com>
2019-01-14 10:54:28 -0800

    Initial skeleton of app.

---
+++ src/main/java/com/learnwithted/kidbank/KidBankApplication.java
@@ -0,0 +1,14 @@
+package com.learnwithted.kidbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidBankApplication.class, args);
+	}
+
+}
+
