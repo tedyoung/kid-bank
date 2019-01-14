119a9bb69ea168d3ea7b6ff4f5084e00cb37f4bb
Ted M. Young <tedyoung@gmail.com>
2019-01-14 11:19:23 -0800

    Formatting 0 balance as $0.00

--- src/main/java/com/learnwithted/kidbank/AccountController.java
+++ src/main/java/com/learnwithted/kidbank/AccountController.java
@@ -1,13 +1,21 @@
 package com.learnwithted.kidbank;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AccountController {
 
   @GetMapping("/")
-  public String viewBalance() {
+  public String viewBalance(Model model) {
+    Account account = new Account();
+
+    model.addAttribute("balance", account.balance());
+    return "account-balance";
+  }
+
+  public String formatAsMoney(int amount) {
     return "$0.00";
   }
 
---
+++ src/test/java/com/learnwithted/kidbank/AccountControllerTest.java
@@ -0,0 +1,16 @@
+package com.learnwithted.kidbank;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountControllerTest {
+
+  @Test
+  public void format0AsMoneyResultsInDollarSignAnd2Decimals() {
+    AccountController accountController = new AccountController();
+
+    assertThat(accountController.formatAsMoney(0))
+        .isEqualTo("$0.00");
+  }
+}
