b31470cccd8c5d1831fe408c1b4340adbe67d94c
Ted M. Young <tedyoung@gmail.com>
2019-02-14 11:28:37 -0800

    Episode 13: Added support for Twilio with configuration and SDK import.

--- src/integrationTest/java/com/learnwithted/kidbank/KidBankApplicationTests.java
+++ src/integrationTest/java/com/learnwithted/kidbank/KidBankApplicationTests.java
@@ -2,16 +2,31 @@ package com.learnwithted.kidbank;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KidBankApplicationTests {
 
+  @Autowired
+  private TwilioConfig twilioConfig;
+
   @Test
   public void contextLoads() {
   }
 
+  @Test
+  public void twilioConfigLoads() {
+    assertThat(twilioConfig.getAccountSid())
+        .isNotBlank()
+        .isEqualTo("AXXXX");
+    assertThat(twilioConfig.getAuthToken())
+        .isNotBlank()
+        .isEqualTo("12345");
+  }
 }
 
---
+++ src/main/java/com/learnwithted/kidbank/TwilioConfig.java
@@ -0,0 +1,27 @@
+package com.learnwithted.kidbank;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "twilio")
+public class TwilioConfig {
+  private String accountSid;
+  private String authToken;
+
+  public String getAccountSid() {
+    return accountSid;
+  }
+
+  public void setAccountSid(String accountSid) {
+    this.accountSid = accountSid;
+  }
+
+  public String getAuthToken() {
+    return authToken;
+  }
+
+  public void setAuthToken(String authToken) {
+    this.authToken = authToken;
+  }
+}
