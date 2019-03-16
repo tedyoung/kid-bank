8f67d30b26c3d675bfba37866cf119c3a369d655
Ted M. Young <tedyoung@gmail.com>
2019-03-15 17:41:27 -0700

    Different way to permit api endpoint to be accessible (apparently .permitAll() doesn't do what we want).

--- src/main/java/com/learnwithted/kidbank/config/Oauth2SecurityConfig.java
+++ src/main/java/com/learnwithted/kidbank/config/Oauth2SecurityConfig.java
@@ -3,6 +3,7 @@ package com.learnwithted.kidbank.config;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,9 +11,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class Oauth2SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().mvcMatchers("/api/sms");
+  }
+
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/", "/error", "/login", "/api/sms")
+        .antMatchers("/", "/error", "/login")
           .permitAll()
         .antMatchers("/deposit", "/spend", "/import")
           .hasRole("PARENT")
