abb68cc631fdb4a34d0658b6dad964891e3aedf9
Ted M. Young <tedyoung@gmail.com>
2019-03-26 13:05:55 -0700

    Season 2 Episode 2 (S2E02) - Add git commit info to "home" page (welcome)

--- src/main/java/com/learnwithted/kidbank/adapter/web/WelcomeController.java
+++ src/main/java/com/learnwithted/kidbank/adapter/web/WelcomeController.java
@@ -1,13 +1,21 @@
 package com.learnwithted.kidbank.adapter.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
+  @Autowired
+  GitProperties gitProperties;
+
   @GetMapping("/")
-  public String welcome() {
+  public String welcome(Model model) {
+    model.addAttribute("commitIdShort", gitProperties.getShortCommitId());
+    model.addAttribute("commitTime", gitProperties.getCommitTime());
     return "welcome";
   }
 
